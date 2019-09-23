00554b6411c46fe0305d4913eae911464838f6a2
wangjianghong01 <51732855+wangjianghong01@users.noreply.github.com>
2019-09-23 21:18:29 +0800

    Update indexJs.js

--- js/indexJs.js
+++ js/indexJs.js
@@ -1,20 +1,26 @@
-var name;
-function tz(id,m){
-	var sp=document.getElementById(id);
-	 name=m;
-	sp.href="../index2/index2.html";
-	return name;
+//公共数据
+var src="../index2/video/";
+//var name=['图1.mp4','图2.mp4','图3.mp4','图5.mp4'];
+
+ 
+//index页面onload加载事件
+function load(){		
+	//点击页面跳转
+	var video=document.getElementsByClassName("sp1");
+	for(var i=0;i<video.length;i++){
+		video[i].onclick= function(){			
+			window.location.href="index2/index2.html";			
+		}
+	}	
 }
-function tz1(id,m){
-	var sp=document.getElementById(id);
-	 name=m;
-	sp.href="index2/index2.html";
-	return name;
-}
-function cheshi(){
-	var tit=document.getElementById("title");
-	tit.innerHTML="视频"+"播放";	
-	var a=$("#vid");
+
+
+
+
+//index2页面onload加载事件
+function load2(){
+	var vidplay=document.getElementById("vidplay");	
+	vidplay.src=src;
 	
 }
 
